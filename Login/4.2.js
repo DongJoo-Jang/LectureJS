7f53b6b46d2cfbe6c2cf58a77e6c65ef9fcbb9f3
DongJooJang <svra0945@gmail.com>
2022-09-14 22:18:12 +0900

    노마드코더 4.2 preventDefault()

---
+++ Login/4.2.js
@@ -0,0 +1,11 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    console.log(event);
+}
+
+loginForm.addEventListener("sumbit", onLoginSubmit )
+
