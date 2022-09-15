db69e27b402076ce6d4631d9484d08fb23838603
DongJooJang <svra0945@gmail.com>
2022-09-15 23:39:20 +0900

    바닐라 JS로 크롬앱 만들기 4.4 수강

--- Login/4.3.js
+++ Login/4.3.js
@@ -2,10 +2,19 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event){
     event.preventDefault();
     console.log(event);
 }
 
+function handleLinkClick(event){
+    event.preventDefault();
+    console.log(event);
+    alert("clicked");
+}
+
 loginForm.addEventListener("submit", onLoginSubmit )
+link.addEventListener("click",handleLinkClick);
 
